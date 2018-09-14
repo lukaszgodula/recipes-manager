eed4770c286f5c4a1b75981be65818d31d49df51
lukaszgodula <lukaszgodula@gmail.com>
2018-09-14 23:48:57 +0200

    ut fix

--- src/app/recipes-list/recipes-list-table/recipes-list-table.component.spec.ts
+++ src/app/recipes-list/recipes-list-table/recipes-list-table.component.spec.ts
@@ -1,14 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatTableModule } from '@angular/material/table';
 
 import { RecipesListTableComponent } from './recipes-list-table.component';
 
 describe('RecipesListTableComponent', () => {
+  const recipesListColumns: string[] = ['name', 'cuisine', 'level'];
   let component: RecipesListTableComponent;
   let fixture: ComponentFixture<RecipesListTableComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RecipesListTableComponent ]
+      declarations: [ RecipesListTableComponent ],
+      imports: [ MatTableModule ]
     })
     .compileComponents();
   }));
@@ -22,4 +25,8 @@ describe('RecipesListTableComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should have proper columns in table`, async(() => {
+    expect(component.displayedColumns).toEqual(recipesListColumns);
+  }));
 });
--- src/app/recipes-list/recipes-list.component.spec.ts
+++ src/app/recipes-list/recipes-list.component.spec.ts
@@ -1,14 +1,29 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Action, Store } from '@ngrx/store';
+import { Subject } from 'rxjs/Subject';
+import { RecipesManagerState } from 'src/app/core/+state/recipes-manager.interfaces';
+import { MockStore } from 'src/mocks/mock-store';
 
 import { RecipesListComponent } from './recipes-list.component';
 
 describe('RecipesListComponent', () => {
   let component: RecipesListComponent;
   let fixture: ComponentFixture<RecipesListComponent>;
+  const actions = new Subject<Action>();
+  const states = new Subject<RecipesManagerState>();
+  // const store = mockStore<RecipesManagerState>({ actions, states });
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RecipesListComponent ]
+      declarations: [ RecipesListComponent ],
+      providers: [
+        {
+          provide: Store,
+          useClass: MockStore
+        }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
