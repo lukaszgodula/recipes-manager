9912cafdf756cc7242edf63be2a849b159ad92e3
lukaszgodula <lukaszgodula@gmail.com>
2018-10-10 15:02:56 +0200

    unsubscribing on destroy and tab size change to 2

--- src/app/core/utils/store.util.ts
+++ src/app/core/utils/store.util.ts
@@ -5,9 +5,9 @@ import { MemoizedSelector } from '@ngrx/store/src/selector';
 import { Observable } from 'rxjs/Observable';
 
 export class StoreUtil {
-    // use to share the same store value across multiple AsyncPipe uses (by default,
-    // AsyncPipe creates new subscription for each pipe occurence)
-    public static select<TStore, TResult>(store: Store<TStore>, selector: MemoizedSelector<TStore, TResult>): Observable<TResult> {
-      return store.select(selector).publishReplay(1).refCount();
-    }
+  // use to share the same store value across multiple AsyncPipe uses (by default,
+  // AsyncPipe creates new subscription for each pipe occurence)
+  public static select<TStore, TResult>(store: Store<TStore>, selector: MemoizedSelector<TStore, TResult>): Observable<TResult> {
+    return store.select(selector).publishReplay(1).refCount();
   }
+}
