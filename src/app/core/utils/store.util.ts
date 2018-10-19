f88d0919916f260038735809ae679910b1b2850b
lukaszgodula <lukaszgodula@gmail.com>
2018-10-19 15:46:10 +0200

    remove deprecated select operator from store util

--- src/app/core/utils/store.util.ts
+++ src/app/core/utils/store.util.ts
@@ -1,13 +1,10 @@
-import 'rxjs/add/operator/publishReplay';
-
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { MemoizedSelector } from '@ngrx/store/src/selector';
 import { Observable } from 'rxjs/Observable';
+import { publishReplay, refCount } from 'rxjs/operators';
 
 export class StoreUtil {
-  // use to share the same store value across multiple AsyncPipe uses (by default,
-  // AsyncPipe creates new subscription for each pipe occurence)
   public static select<TStore, TResult>(store: Store<TStore>, selector: MemoizedSelector<TStore, TResult>): Observable<TResult> {
-    return store.select(selector).publishReplay(1).refCount();
+    return store.pipe(select(selector), publishReplay(1), refCount());
   }
 }
