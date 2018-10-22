4f8db30a387d66e0ff65216df4d30aca2802b823
lukaszgodula <lukaszgodula@gmail.com>
2018-10-22 11:52:36 +0200

    change home component selector

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'home',
+  selector: 'recipes-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
