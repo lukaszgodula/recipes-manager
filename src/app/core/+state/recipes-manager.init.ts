8585c51ac60448ed6213a4c3f8b0a8b93cda592e
lukaszgodula <lukaszgodula@gmail.com>
2018-10-24 14:46:18 +0200

    build fix

--- src/app/core/+state/recipes-manager.init.ts
+++ src/app/core/+state/recipes-manager.init.ts
@@ -4,7 +4,6 @@ export const initialState: RecipesManagerState = {
     isAppLoading: true,
     authInProgress: false,
     user: null,
-    tokenId: null,
     recipesList: [],
     recipeDetails: null
 };
