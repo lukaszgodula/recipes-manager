35e3edf854a0a09f93bfc5ca641dd34880564a47
lukaszgodula <lukaszgodula@gmail.com>
2018-10-25 09:43:20 +0200

    add recipe form component

---
+++ src/app/add-recipe/add-recipe-form/add-recipe-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'add-recipe-form',
+  templateUrl: './add-recipe-form.component.html',
+  styleUrls: ['./add-recipe-form.component.scss']
+})
+export class AddRecipeFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/add-recipe/add-recipe.module.ts
+++ src/app/add-recipe/add-recipe.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AddRecipeRoutingModule } from './add-recipe-routing.module';
 import { AddRecipeComponent } from './add-recipe.component';
+import { AddRecipeFormComponent } from './add-recipe-form/add-recipe-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AddRecipeRoutingModule
   ],
-  declarations: [AddRecipeComponent]
+  declarations: [AddRecipeComponent, AddRecipeFormComponent]
 })
 export class AddRecipeModule { }
