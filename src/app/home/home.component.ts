df7a140558902cfc923c9eccfe60d37b69a748cf
Lukasz Godula <lgodu@softserveinc.com>
2018-11-09 09:54:22 +0100

    before prod changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,8 @@ import { RecipesManagerState } from 'src/app/core/+state/recipes-manager.interfa
 import { FromRecipesManagerState } from 'src/app/core/+state/recipes-manager.selectors';
 import { StoreUtil } from 'src/app/core/utils/store.util';
 
+import { RecipesListItem } from '../core/models/recipes-list';
+
 @Component({
   selector: 'recipes-home',
   templateUrl: './home.component.html',
@@ -13,10 +15,12 @@ import { StoreUtil } from 'src/app/core/utils/store.util';
 })
 export class HomeComponent implements OnInit {
   public isAppLoading: Observable<boolean>;
+  public recipesListItems: Observable<RecipesListItem[]>;
 
   constructor(private router: Router,
     private store: Store<RecipesManagerState>) {
     this.isAppLoading = StoreUtil.select(this.store, FromRecipesManagerState.isAppLoading);
+    this.recipesListItems = StoreUtil.select(this.store, FromRecipesManagerState.recipesList);
   }
 
   ngOnInit() {
