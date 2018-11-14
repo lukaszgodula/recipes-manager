ca262f0e7a346ad34288a4f1915372abf3f66da9
Lukasz Godula <lgodu@softserveinc.com>
2018-11-14 14:49:56 +0100

    ut update

--- src/app/core/services/service-worker.service.spec.ts
+++ src/app/core/services/service-worker.service.spec.ts
@@ -1,9 +1,18 @@
 import { TestBed } from '@angular/core/testing';
+import { SwUpdate } from '@angular/service-worker';
+import { SwUpdateMock } from 'src/testing/swupdate.mock';
 
+import { RecipesManagerService } from './recipes-manager.service';
 import { ServiceWorkerService } from './service-worker.service';
 
 describe('ServiceWorkerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      ServiceWorkerService,
+      { provide: SwUpdate, useClass: SwUpdateMock },
+      { provide: RecipesManagerService, useValue: {} }
+    ]
+  }));
 
   it('should be created', () => {
     const service: ServiceWorkerService = TestBed.get(ServiceWorkerService);
